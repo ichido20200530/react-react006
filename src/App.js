8db3a7c36afa97d87e9a37553889c7cc43b054dc
ichido <ichido@live.jp>
2020-06-06 19:27:27 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+  msgStyle1 = {
+    fontSize:"24pt",
+    color:"#900",
+    margin:"20px 0px",
+    padding:"5px",
+    borderBottom:"2px solid #900"
+  }
+  msgStyle2 = {
+    fontSize:"20pt",
+    color:"white",
+    backgroundColor:"#900",
+    margin:"20px 0px",
+    padding:"5px",
+    borderBottom:"2px solid #900"
+  }
+  btnStyle = {
+    fontSize:"16pt",
+    padding:"10px"
+  }
+
+  constructor (props){
+    super(props);
+    this.state = {
+      counter: 0,
+      msg: 'count start!',
+      flg: true,
+    };
+    this.doAction = this.doAction.bind(this);
+  }
+
+  doAction(){
+    this.setState((state) => ({
+      counter: state.counter + 1,
+      msg: state.counter,
+      flg: !state.flg
+    }));
+  }
+
+  render(){
+    return <div>
+      <h1>React</h1>
+      {
+        this.state.flg ?
+        <p style={this.msgStyle1}>count: {this.state.msg}</p>
+        :
+        <p style={this.msgStyle2}>{this.state.msg}です。</p>
+      }
+      <button style={this.btnStyle} onClick={this.doAction}>Click</button>
+    </div>
+  }
+
+}
